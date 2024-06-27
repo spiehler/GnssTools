1255fd55509c61d90b6045b1d54974dc1a2435ca
Stefan Piehler <stefan.piehler@pistenbully.com>
2024-06-27 16:23:51 +0200

    Added Bluetooth LE

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 using GnssTools;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazor.Bluetooth;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddBluetoothNavigator();
 
 await builder.Build().RunAsync();
