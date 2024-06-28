f3ba99485761e4f1c4be59213aaa6dc7f03a141f
Stefan Piehler <stefan.piehler@pistenbully.com>
2024-06-28 21:19:48 +0200

    Added Radzen Components

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using GnssTools;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazor.Bluetooth;
+using Radzen;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -10,5 +11,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddBluetoothNavigator();
+builder.Services.AddRadzenComponents();
 
 await builder.Build().RunAsync();
